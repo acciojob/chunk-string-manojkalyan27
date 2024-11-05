37848acc52ee7674450b129107a01d7023615217
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-05 21:37:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function stringChop(str, size) {
-  // your code here
-}
+    // your code here
+    if(!str || size <= 0){
+        return [];
+    }
+    const chunks = [];
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+    for(let i=0;i<=str.length;i=i+size){
+       chunks.push(str.slice(i,i+size));
+    }
+     return chunks; 
+  }
+  
+  // Do not change the code below
+  const str = prompt("Enter String.");
+  const size = prompt("Enter Chunk Size.");
+  alert(stringChop(str, size));
+  
